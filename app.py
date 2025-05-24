ca9a5dd6b913075903a07ca5f60704f79c95bf36
DenysDt <138673839+DenysDt@users.noreply.github.com>
2025-05-24 18:58:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+import pymongo
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+api = Api(app)
+
+client = pymongo.MongoClient("mongodb+srv://DenysDt:bGbPB4bFVK3L8MU@cluster0.1xcwxnh.mongodb.net/")
+mydb = client["mydatabase"]
+myposts = mydb["posts"]
+
+class Hello(Resource):
+
+    def get(self):
+        postid = request.args.get('postid')
+        found_post = myposts.find_one({"_id": postid})
+        return jsonify(found_post)
+
+    def post(self):
+        data = request.get_json()     # status code
+        return jsonify({'data': data}), 201
+api.add_resource(Hello, '/')
