fc9cd898e54f41dd763672215a9989062c618bff
DenysDt <138673839+DenysDt@users.noreply.github.com>
2025-05-30 22:52:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request, render_template
 from flask_restful import Resource, Api
 import pymongo
+import uuid
 
 app = Flask(__name__)
 
 api = Api(app)
 
-client = pymongo.MongoClient("mongodb+srv://DenysDt:bGbPB4bFVK3L8MU@cluster0.1xcwxnh.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient(
+    "mongodb+srv://DenysDt:bGbPB4bFVK3L8MU@cluster0.1xcwxnh.mongodb.net/?retryWrites=true&w=majority")
 mydb = client["mydatabase"]
 myposts = mydb["posts"]
 myusers = mydb["users"]
@@ -16,22 +18,23 @@ data_retrieve = mydata.find_one({"_id": "0"})
 postID = int(data_retrieve["postID"])
 userID = int(data_retrieve["userID"])
 
+
 class postsearch(Resource):
 
     def get(self):
-        
         postid = request.args.get('postid')
         found_post = myposts.find_one({"_id": int(postid)})
         return jsonify(found_post)
 
     def post(self):
-        data = request.get_json()     # status code
+        data = request.get_json()  # status code
         return jsonify({'data': data}), 201
 
 
 class usercreate(Resource):
     def get(self):
-        client = pymongo.MongoClient("mongodb+srv://DenysDt:bGbPB4bFVK3L8MU@cluster0.1xcwxnh.mongodb.net/?retryWrites=true&w=majority")
+        client = pymongo.MongoClient(
+            "mongodb+srv://DenysDt:bGbPB4bFVK3L8MU@cluster0.1xcwxnh.mongodb.net/?retryWrites=true&w=majority")
         mydb = client["mydatabase"]
         myusers = mydb["users"]
         mydata = mydb["data"]
@@ -41,15 +44,18 @@ class usercreate(Resource):
             user = request.args.get('user')
             password = request.args.get('password')
             userID = int(data_retrieve["userID"])
-            myusers.insert_one({"_id": userID, "name": user, "pass": password, "desc": "bio", "mod": False, "rep": 5, "ban": False, "admin": False, "namecol": "white", "test": "False"})
+            myusers.insert_one(
+                {"_id": userID, "name": user, "pass": password, "desc": "bio", "mod": False, "rep": 5, "ban": False,
+                 "admin": False, "namecol": "white", "test": "False"})
             userID += 1
             mydata.update_one({"_id": "0"}, {"$set": {"userID": str(userID)}})
             return jsonify({"message": "success"})
         except Exception as e:
             print(f"ERROR: {e}")
             return jsonify({"message": "error"})
+
     def post(self):
-        data = request.get_json()     # status code
+        data = request.get_json()  # status code
         return jsonify({'data': data}), 201
 
 
@@ -66,11 +72,11 @@ class login(Resource):
                 return jsonify({"message": "!pass"})
         else:
             return jsonify({"message": "!user"})
-            
 
     def post(self):
-        data = request.get_json()     # status code
+        data = request.get_json()  # status code
         return jsonify({'data': data}), 201
+
 
 class health(Resource):
 
@@ -78,14 +84,15 @@ class health(Resource):
         return "Hello!"
 
     def post(self):
-        data = request.get_json()     # status code
+        data = request.get_json()  # status code
         return jsonify({'data': data}), 201
 
 
 class postcreate(Resource):
 
     def get(self):
-        client = pymongo.MongoClient("mongodb+srv://DenysDt:bGbPB4bFVK3L8MU@cluster0.1xcwxnh.mongodb.net/?retryWrites=true&w=majority")
+        client = pymongo.MongoClient(
+            "mongodb+srv://DenysDt:bGbPB4bFVK3L8MU@cluster0.1xcwxnh.mongodb.net/?retryWrites=true&w=majority")
         mydb = client["mydatabase"]
         myposts = mydb["posts"]
         myusers = mydb["users"]
@@ -114,8 +121,9 @@ class postcreate(Resource):
             return jsonify({"message": "error"})
 
     def post(self):
-        data = request.get_json()     # status code
+        data = request.get_json()  # status code
         return jsonify({'data': data}), 201
+
 
 class freejob(Resource):
 
@@ -123,9 +131,8 @@ class freejob(Resource):
         return "The fuck u expect🙏🙏🙏  Bro's getting scammed so easily🤞❌❌"
 
     def post(self):
-        data = request.get_json()     # status code
+        data = request.get_json()  # status code
         return jsonify({'data': data}), 201
-
 
 
 class preBattleShipsRequest(Resource):
@@ -133,52 +140,32 @@ class preBattleShipsRequest(Resource):
     def get(self):
         client = pymongo.MongoClient("mongodb+srv://DenysDt:bGbPB4bFVK3L8MU@cluster0.1xcwxnh.mongodb.net/?retryWrites=true&w=majority")
         mydb = client["mydatabase"]
-        mybattles = client["battles"]
+        mybattles = mydb["battles"]
 
-        #data_retrieve = mydata.find_one({"_id": "0"})
+        # data_retrieve = mydata.find_one({"_id": "0"})
         try:
             ships = request.args.get('ships')
             battleid = request.args.get("battleid")
             playerid = request.args.get("playerid")
             amount = ships.count(",")
             x = ships.partition(",")
-            xlen = len(x[0])
-            if xlen != 2:
-                print(f"Error! Square {x[0]} doesn't exist!")
-                return jsonify({"message": "error"})
+            if mybattles.find_one({"squares": {"$eq": x[0]}}):
+                squares_info = mybattles.find_one({"_id": battleid})
+                if squares_info["squares"][x[0]][playerid] == "false":
+                    mybattles.update_one({"_id": battleid}, {"$set": {x[0]: {playerid: "true"}}})
             else:
-                if x[0][0] == "A" or x[0][0] == "B" or x[0][0] == "C" or x[0][0] == "D":
-                    if x[0][1] == "1" or x[0][1] == "2" or x[0][1] == "3" or x[0][1] == "4":
-                        ...
-                    else:
-                        print(f"Error! Square {x[0]} doesn't exist!")
-                        return jsonify({"message": "error"})
-                else:
-                    print(f"Error! Square {x[0]} doesn't exist!")
-                    return jsonify({"message": "error"})
-
-            
+                return jsonify({"message": "error"})
 
             for loop in range(amount):
                 x = x[2].partition(",")
-                xlen = len(x[0])
-                if xlen != 2:
-                    print(f"Error! Square {x[0]} doesn't exist!")
-                    return jsonify({"message": "error"})
-                else:
-                    if x[0][0] == "A" or x[0][0] == "B" or x[0][0] == "C" or x[0][0] == "D":
-                        if x[0][1] == "1" or x[0][1] == "2" or x[0][1] == "3" or x[0][1] == "4":
-                            ...
-                        else:
-                            print(f"Error! Square {x[0]} doesn't exist!")
-                            return jsonify({"message": "error"})
-                    else:
-                        print(f"Error! Square {x[0]} doesn't exist!")
-                        return jsonify({"message": "error"})
 
-                if true:
-                    ...
-                mybattles.update_one({"_id": battleid}, {"$set": {x[0]: str(postID)}})
+                if mybattles.find_one({"squares": {"$eq": x[0]}}):
+                    squares_info = mybattles.find_one({"_id": battleid})
+                    if squares_info["squares"][x[0]][playerid] == "false":
+                        mybattles.update_one({"_id": battleid}, {"$set": {x[0]: {playerid: "true"}}})
+                else:
+                    return jsonify({"message": "error"})
+
 
             return jsonify({"message": "success"})
 
@@ -188,9 +175,46 @@ class preBattleShipsRequest(Resource):
             return jsonify({"message": "error"})
 
     def post(self):
-        data = request.get_json()     # status code
+        data = request.get_json()  # status code
         return jsonify({'data': data}), 201
 
+
+class battlejoin(Resource):
+
+    def get(self):
+        client = pymongo.MongoClient("mongodb+srv://DenysDt:bGbPB4bFVK3L8MU@cluster0.1xcwxnh.mongodb.net/?retryWrites=true&w=majority")
+        mydb = client["mydatabase"]
+        mybattles = mydb["battles"]
+
+
+        battleid = request.args.get("battleid")
+        if mybattles.find_one({"_id": {"$eq": battleid}}):
+            battle_data = mybattles.find_one({"_id": battleid})
+            if battle_data["players"] >= 2:
+                return jsonify({"message": "!full"})
+            else:
+                if battle_data["players"] == 0:
+                    userid_gen = str(uuid.uuid4())
+                    players_count = battle_data["players"]
+                    players_count += 1
+                    mydata.update_one({"_id": battleid}, {"$set": {"players": players_count}})
+                    mydata.update_one({"_id": battleid}, {"$set": {"player1": userid_gen}})
+                    return jsonify({"message": "success", "gen_userid": userid_gen})
+                elif battle_data["players"] == 1:
+                    userid_gen = str(uuid.uuid4())
+                    players_count = battle_data["players"]
+                    players_count += 1
+                    mydata.update_one({"_id": battleid}, {"$set": {"players": players_count}})
+                    mydata.update_one({"_id": battleid}, {"$set": {"player2": userid_gen}})
+                    return jsonify({"message": "success", "gen_userid": userid_gen})
+
+                else:
+                    return jsonify({"message": "!full"})
+
+
+    def post(self):
+        data = request.get_json()  # status code
+        return jsonify({'data': data}), 201
 
 
 
@@ -200,10 +224,8 @@ def index():
 
 
 api.add_resource(health, "/health")
-#api.add_resource(postsearch, '/post')
-#api.add_resource(usercreate, "/signup")
-#api.add_resource(login, "/login")
-#api.add_resource(postcreate, "/postcreate")
-#api.add_resource(freejob, "/freejob")
-
-
+# api.add_resource(postsearch, '/post')
+# api.add_resource(usercreate, "/signup")
+# api.add_resource(login, "/login")
+# api.add_resource(postcreate, "/postcreate")
+api.add_resource(freejob, "/freejob")
